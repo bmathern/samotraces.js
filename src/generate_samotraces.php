480051defc9921260aba1cdb6be3a9a559f52f98
bmathern <benoit.mathern@liris.cnrs.fr>
2014-01-20 11:47:09 +0100

    Updated doc

--- src/generate_samotraces.php
+++ src/generate_samotraces.php
@@ -35,23 +35,6 @@ do {
 } while($dirs = glob($dir, GLOB_ONLYDIR));
 } 
 
-/*
-echo(file_get_contents('Lib/EventHandler.js'));
-echo(file_get_contents('Lib/Collecteur.js'));
-echo(file_get_contents('Lib/addBehaviour.js'));
-echo(file_get_contents('Lib/Obsel.js'));
-echo(file_get_contents('Lib/Selector.js'));
-echo(file_get_contents('Lib/DemoTrace.js'));
-echo(file_get_contents('Lib/KtbsTrace.js'));
-echo(file_get_contents('Lib/KtbsBogueTrace.js'));
-echo(file_get_contents('Lib/Ktbs4jsTrace.js'));
-echo(file_get_contents('Lib/KtbsBase.js'));
-echo(file_get_contents('Lib/Ktbs.js'));
-echo(file_get_contents('Lib/Timer.js'));
-echo(file_get_contents('Lib/SelfUpdatingTimer.js'));
-echo(file_get_contents('Lib/TimeWindow.js'));
-echo(file_get_contents('Lib/WindowState.js'));
-*/
 export_all_js('Lib/');
 export_all_js('Widgets/');
 export_all_js('UIComponents/');
