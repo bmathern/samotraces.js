0da5ac4807c56cfc76c3e8fde67d01ebf6e4a38b
bmathern <benoit.mathern@liris.cnrs.fr>
2014-02-24 14:35:51 +0100

    Added Samotraces.log and Samotraces.debug functions

--- src/generate_samotraces.js
+++ src/generate_samotraces.js
@@ -8,28 +8,41 @@
         window.Samotraces = factory(jQuery);
     }
 }(function ($) {
-	/**
-	 * @namespace Samotraces
-	 */
-	Σ = {};
-	var Samotraces = Σ;
+	var Samotraces = {};
 	/**
 	 * @property {Boolean} [debug=false]
 	 */
-	Σ.debug = false;
+	Samotraces.debug = false;
 
 	/**
 	 * Library of the Objects of Samotraces
 	 * @namespace Samotraces.Lib
 	 */
-	Σ.Lib = {};
-	Σ.UIComponents = {};
+	Samotraces.Lib = {};
+	/**
+	 * Library of UI components for Samotraces
+	 * @namespace Samotraces.UIComponents
+	 */
+	Samotraces.UIComponents = {};
 	/**
 	 * Set of Widgets of Samotraces
 	 * @namespace Samotraces.Widgets
 	 */
-	Σ.Widgets = {};
+	Samotraces.Widgets = {};
 
+	Samotraces.log = function() {
+		if(window.console) {
+			var class_name = (this!==undefined)?[this.constructor.name]:[];
+            window.console.log.apply(console, [ "Samotraces.js" ].concat(class_name.slice.call(arguments)));
+		}
+	};
+	Samotraces.debug = function() {
+		if(window.console && Samotraces.debug) {
+			var class_name = (this!==undefined)?[this.constructor.name]:[];
+            window.console.log.apply(console, [ "Samotraces.js-debug" ].concat(class_name.slice.call(arguments)));
+		}
+	};
+	
 	// ALL SRC FILES INCLUDED HERE --- SEE PHP	
 
 	return Samotraces;
--- src/generate_samotraces.php
+++ src/generate_samotraces.php
@@ -8,31 +8,40 @@
         window.Samotraces = factory(jQuery);
     }
 }(function ($) {
-	/**
-	 * @namespace Samotraces
-	 */
-	Σ = {};
-	var Samotraces = Σ;
+	var Samotraces = {};
 	/**
 	 * @property {Boolean} [debug=false]
 	 */
-	Σ.debug = false;
+	Samotraces.debug = false;
 
 	/**
 	 * Library of the Objects of Samotraces
 	 * @namespace Samotraces.Lib
 	 */
-	Σ.Lib = {};
+	Samotraces.Lib = {};
 	/**
 	 * Library of UI components for Samotraces
 	 * @namespace Samotraces.UIComponents
 	 */
-	Σ.UIComponents = {};
+	Samotraces.UIComponents = {};
 	/**
 	 * Set of Widgets of Samotraces
 	 * @namespace Samotraces.Widgets
 	 */
-	Σ.Widgets = {};
+	Samotraces.Widgets = {};
+
+	Samotraces.log = function() {
+		if(window.console) {
+			var class_name = (this!==undefined)?[this.constructor.name]:[];
+            window.console.log.apply(console, [ "Samotraces.js" ].concat(class_name.slice.call(arguments)));
+		}
+	};
+	Samotraces.debug = function() {
+		if(window.console && Samotraces.debug) {
+			var class_name = (this!==undefined)?[this.constructor.name]:[];
+            window.console.log.apply(console, [ "Samotraces.js-debug" ].concat(class_name.slice.call(arguments)));
+		}
+	};
 	
 <?php
 
@@ -49,7 +58,7 @@ do {
 
 export_all_js('Lib/');
 export_all_js('Widgets/');
-export_all_js('UIComponents/');
+//export_all_js('UIComponents/');
 
 ?>
 	return Samotraces;
